37b817160f484fd40e989e20bcb73a31f7b29ae2
Nolan <nolan.nn235@gmail.com>
2021-07-10 12:24:41 +0700

    first commit

---
+++ build/loginRoutes.js
@@ -0,0 +1,12 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var express_1 = require("express");
+function requireAuth(req, res, next) {
+    if (req.session && req.session.loggedIn) {
+        next();
+        return;
+    }
+    res.status(403);
+    res.send('Not permitted');
+}
+var router = express_1.Router();
---
+++ src/loginRoutes.ts
@@ -0,0 +1,17 @@
+import { Router, Request, Response, NextFunction } from 'express';
+
+interface RequestWithBody extends Request {
+    body: { [key: string]: string | undefined};
+}
+
+function requireAuth(req: Request, res: Response, next: NextFunction): void {
+    if (req.session && req.session.loggedIn) {
+        next();
+        return;
+    }
+
+    res.status(403);
+    res.send('Not permitted');
+}
+
+const router = Router();
