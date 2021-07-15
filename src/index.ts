9cf4b9dacb7df126e8c56b7c77ebfd6955a54fd6
Nolan <nolan.nn235@gmail.com>
2021-07-16 06:57:16 +0700

    update controller router

--- src/index.ts
+++ src/index.ts
@@ -2,12 +2,14 @@ import express, { Request, Response } from 'express'
 import { router } from './routes/loginRoutes';
 import bodyParser from 'body-parser';
 import cookieSession from 'cookie-session';
-
+import  {router as controllerRouter} from './controllers/decorators/controller'
+import './controllers/loginController'
 const app = express();
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(cookieSession({keys: ['fdfsd']}));
 app.use(router);
+app.use(controllerRouter)
 
 app.listen(3000, () => {
     console.log('Listen on port 3000');
